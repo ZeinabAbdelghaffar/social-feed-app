b7fbbc713aeced7b97f1a13d7ef2abb0ae2941b1
Zeinab Abdelghaffar <zeinanabdo88@gmail.com>
2024-06-01 21:59:46 +0300

    Modify Show Less and Show more options for the post content

--- components/CreatorArea.vue
+++ components/CreatorArea.vue
@@ -11,7 +11,13 @@
           <p class="creator-name">{{ creator.firstName }} {{ creator.lastName }}</p>
           <p class="creator-username">@{{ creator.username }}</p>
           <h2 class="post-title">{{ title }}</h2>
-          <p class="post-content">{{ content }}</p>
+          <p class="post-content" :class="{ 'limited-content': showLimitedContent }">{{ showLimitedContent ? limitedContent : content }}</p>
+          <span v-if="content.length > 50 && !showFullContent">
+            <a class="show-more" @click="showFullContent = true">Show More</a>
+          </span>
+          <span v-else-if="content.length > 50 && showFullContent">
+            <a class="show-less" @click="showFullContent = false">Show Less</a>
+          </span>
         </div>
       </v-col>
     </v-row>
@@ -33,6 +39,58 @@ export default {
       type: String,
       default: ''
     }
+  },
+  data() {
+    return {
+      showFullContent: false
+    };
+  },
+  computed: {
+    limitedContent() {
+      if (this.content.length > 50) {
+        return this.content.substring(0, 50) + '…';
+      } else {
+        return this.content;
+      }
+    },
+    showLimitedContent() {
+      return this.content.length > 50 && !this.showFullContent;
+    }
   }
 };
 </script>
+
+<style scoped>
+.creator-area {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.creator-info {
+  font-size: 16px;
+}
+
+.post-content {
+  margin-top: 10px;
+}
+
+.limited-content {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.show-more,
+.show-less {
+  color: #007bff;
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.show-more:hover,
+.show-less:hover {
+  color: #0056b3;
+}
+</style>
